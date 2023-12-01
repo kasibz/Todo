a2753058b4ecfb967522cbc0c427d299f56b67b9
kasib <kasibzxc@gmail.com>
2023-12-01 15:57:00 -0500

    made successful connection to mysql in db_test.py

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
+# db connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/todo'
 
 @app.route("/")
 def hello():
