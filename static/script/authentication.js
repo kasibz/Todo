28701a0dc21f2288a448beadfd65078a473655a8
kasib <kasibzxc@gmail.com>
2023-12-04 15:24:06 -0500

    error handled to get forms working

--- static/script/authentication.js
+++ static/script/authentication.js
@@ -1,8 +1,33 @@
-let loginForm = document.getElementById('login')
-loginForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    // console.log(event.target.username.value)
-    // console.log(event.target.password.value)
+try {
+    let loginForm = document.getElementById('login')
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        // console.log(event.target.username.value)
+        // console.log(event.target.password.value)
+    })
+} catch {
+    console.log("Not on login page")
+}
 
-    
-})
+try {
+    let logoutForm = document.getElementById('logout')
+    logoutForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        console.log(event.target.username.value)
+        console.log(event.target.password.value)
+    })
+} catch {
+    console.log("Not on logout page")
+}
+
+try {
+    let signupForm = document.getElementById('signup')
+    signupForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        console.log(event.target.username.value)
+        console.log(event.target.password.value)
+    })    
+} catch {
+    console.log("Not on signup")
+}
+
